ea18c24ad0e51f9e473b3528aa82e4882185a26f
sumit <suuummiiit@gmail.com>
2025-01-25 06:52:38 +0530

    update

--- PythonBackend/app.py
+++ PythonBackend/app.py
@@ -1,30 +1,110 @@
 from flask import Flask, request, jsonify
 from diet import generate_diet
-
+from scan_packed_food import scan
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 # Route to handle requests from the Node.js backend
 @app.route('/plan_diet', methods=['POST'])
 def plan_diet():
+    print("Received request to plan diet")
     try:
-        print("Received request to plan diet")
         # Parse JSON data from the request
         user_profile_data = request.json
         if not user_profile_data:
             return jsonify({"error": "Invalid or missing JSON data"}), 400
+        
+        print("User Profile Data:", user_profile_data)
 
         # Pass the data to the generate function
         result = generate_diet(user_profile_data)
 
         # Return the result as a JSON response
         print("Result:", result)
-        print(type(result[0]))
         return jsonify(result), 200
 
     except Exception as e:
         # Handle errors and return a 500 status
+        print("Error:", e)
+        return jsonify({"error": str(e)}), 500
+from flask import Flask, request, jsonify
+import os
+import json
+from scan_packed_food import scan
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+
+
+@app.route('/scan_img', methods=['POST'])
+def scan_img():
+    print("Received request to scan image")
+    try:
+        # Log incoming request data for debugging
+        print("Request Files:", request.files)
+        print("Request Form:", request.form)
+
+        # Check if an image file is included in the request
+        if 'image' not in request.files:
+            print("Error: No image file provided")
+            return jsonify({"error": "No image file provided"}), 400
+
+        # Get the uploaded image file
+        image_file = request.files['image']
+
+        # Validate the file type
+        if not image_file.filename.endswith(('.png', '.jpg', '.jpeg')):
+            print("Error: Invalid file type. Only PNG, JPG, and JPEG are allowed.")
+            return jsonify({"error": "Invalid file type. Only PNG, JPG, and JPEG are allowed."}), 400
+
+        # Save the file to the uploads folder
+        file_path = os.path.join(UPLOAD_FOLDER, image_file.filename)
+        image_file.save(file_path)
+
+        # Retrieve and validate the description
+        description = request.form.get("description")
+        if not description:
+            print("Error: No description provided")
+            return jsonify({"error": "No description provided"}), 400
+
+        # Retrieve and parse user_Details
+        user_details = request.form.get("user_Details")
+        if not user_details:
+            print("Error: No user_Details provided")
+            return jsonify({"error": "No user_Details provided"}), 400
+        user_details = json.loads(user_details)  # Convert JSON string to dictionary
+
+        # Retrieve and parse user_Diet
+        user_diet = request.form.get("user_Diet")
+        if not user_diet:
+            print("Error: No user_Diet provided")
+            return jsonify({"error": "No user_Diet provided"}), 400
+        user_diet = json.loads(user_diet)  # Convert JSON string to dictionary
+
+        # Log received data
+        print("Description:", description)
+        print("User Details:", user_details)
+        print("User Diet:", user_diet)
+
+        # Call the scan function with the received data
+        result = scan(file_path, description, user_details, user_diet)
+
+        print(result)
+
+        # Return the result
+        return jsonify(result), 200
+
+    except Exception as e:
+        print("Error:", e)
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
--- PythonBackend/scan_packed_food.py
+++ PythonBackend/scan_packed_food.py
@@ -11,9 +11,19 @@ key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=key)
 Model = "gpt-4o-mini"
 
+
+system_message = """
+You are an expert at structured data extraction. You will be given an image of packed food and you must convert it into the given structue by extracting the ingredients and the nutritional facts, for nutritional facts the amount should be equal to the amount of food the the user is consuming, if not mentioned then consider the whole packet (per serve). Also the values of in the nutrition_fact should be in grams, if given in miligrams then convert it to grams. Use g for grams unit
+You have also been provided the user_profile, based on the medical history and allergies and other useful info, find out whether it is good for the user to eat it, and give a feedback and its advantages or disagvantages.
+You have also been provided with the user's diet, based on the user's diet and give you final thoughts about whether the user should eat it
+"""
+
+
 class food_info_extraction_format(BaseModel):
     nutritional_facts: list[str, str]
     ingredients: list[str]
+    feedback: str
+    final_thoughts: str
 
 # Function to encode the image
 def encode_image(image_path):
@@ -21,7 +31,7 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-def get_data(image_path, user_prompt):
+def get_data(image_path, user_prompt, user_profile, user_diet):
     base64_image = encode_image(image_path)
 
     completion = client.beta.chat.completions.parse(
@@ -29,7 +39,7 @@ def get_data(image_path, user_prompt):
         messages=[
             {
                 "role": "system",
-                "content": "You are an expert at structured data extraction. You will be given an image of packed food and you must convert it into the given structue by extracting the ingredients and the nutritional facts, for nutritional facts the amount should be equal to the amount of food the the user is consuming, if not mentioned then consider the whole packet (per serve). Also the values of in the nutrition_fact should be in float and in grams, if given in miligrams then convert it to grams"
+                "content": system_message
             },
             {
                 "role": "user",
@@ -37,7 +47,7 @@ def get_data(image_path, user_prompt):
                     {
                         "type": "text",
                         # "text": "This is maggie noodles, I am gonna eat all of this",
-                        "text": user_prompt,
+                        "text": f"{user_prompt} {user_profile} {user_diet}",
                     },
                     {
                         "type": "image_url",
@@ -62,30 +72,56 @@ def clean_data(data):
     parsed_data = json.loads(data)
     nutritional_facts = parsed_data.get("nutritional_facts", [])
     ingredients = parsed_data.get("ingredients", [])
+    feedback = parsed_data.get("feedback", [])
+    final_thoughts = parsed_data.get("final_thoughts", [])
 
     # Process the list into a dictionary
     nutritional_facts_dict = {}
     for fact in nutritional_facts:
         fact = fact.lstrip("-").strip()
         key, value = fact.split(": ")
-        value = value.replace("g", "").strip()
-        nutritional_facts_dict[key] = float(value)
-        # nutritional_facts_dict[key] = value
+        # value = value.replace("g", "").strip()
+        nutritional_facts_dict[key] = value
 
     # Convert to JSON
     nutritional_facts_json = json.dumps(nutritional_facts_dict, indent=2)
-    return nutritional_facts_json, ingredients
+    return nutritional_facts_json, ingredients, feedback, final_thoughts
 
-def scan(image_path, user_prompt):
-    data = get_data(image_path, user_prompt)
-    nutritional_facts_json, ingredients = clean_data(data)
-    return nutritional_facts_json, ingredients
+def scan(image_path, user_prompt, user_profile, user_diet):
+    data = get_data(image_path, user_prompt, user_profile, user_diet)
+    # print(data)
+    nutritional_facts_json, ingredients, feedback, final_thoughts = clean_data(data)
+    return [nutritional_facts_json, ingredients, feedback, final_thoughts]
     
 
 
 if __name__ == "__main__":
     image_path = input("Enter the path to the image: ")
     user_prompt = input("Enter the user prompt: ")
-    nutritional_facts_json, ingredients = scan(image_path, user_prompt)
-    print(nutritional_facts_json)
-    print(ingredients)
+    # nutritional_facts_json, ingredients = scan(image_path, user_prompt)
+    # print(nutritional_facts_json)
+    # print(ingredients)
+
+    # example
+    user_profile = {
+        "Age": "20",
+        "weight (kg)": "70",
+        "Height (cm)": "170",
+        "Gender": "Male",
+        "Activity Level": "Moderately Active",
+        "Dietary Preferences": "Vegetarian",
+        "Allergies": "None",
+        "Taste Preferences": "Spicy",
+        "Medical History": "None",
+    }
+
+
+    user_diet = {"nutrients":{"Calories":2500,"Proteins":80,"Carbohydrates":350,"Fats":70,"Sugar":50,"Sodium":2000,"Fiber":30,"Vitamin A":900,"Vitamin C":90,"Calcium":1000,"Iron":14,"Magnesium":400,"Potassium":3500,"Vitamin B12":2.4},"mealSuggestions":["Meal 1: Tofu stir-fry with mixed vegetables (broccoli, bell peppers, and carrots) served with brown rice. Seasoned with soy sauce and sesame oil.","Meal 2: Whole grain pasta with marinara sauce, lentils, and spinach topped with nutritional yeast.","Meal 3: Smoothie with banana, spinach, almond milk, and a scoop of plant-based protein powder.","Meal 4: Quinoa salad with chickpeas, cherry tomatoes, cucumber, avocado, and a lemon-tahini dressing.","Meal 5: Vegetable curry with sweet potatoes and served with basmati rice and side of mixed greens salad with vinaigrette."]}
+
+
+
+    result = scan(image_path, user_prompt, user_profile, user_diet)
+    print(result[0])
+    print(result[1])
+    print(result[2])
+    print(result[3])
